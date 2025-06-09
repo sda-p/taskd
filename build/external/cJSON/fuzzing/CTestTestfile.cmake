882f653e63dc6a4a891acfc5f10d537590162a69
ssa <nextcris@hotmail.com>
2025-06-09 08:53:36 +0100

    lol2

--- build/external/cJSON/fuzzing/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/cris/Documents/ssAGI/taskd/external/cJSON/fuzzing
-# Build directory: /home/cris/Documents/ssAGI/taskd/build/external/cJSON/fuzzing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
