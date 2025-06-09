3bada7bdf88dc2aab4cee682d90819490c1db04b
ssa <nextcris@hotmail.com>
2025-06-09 08:46:59 +0100

    Updated build script.

---
+++ build/CMakeFiles/xxhash_build.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xxhash_build"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/xxhash_build.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
